b828106c1246ce25bb4ce8a6078273a213461b73
HessianPL <git@lukaszdragon.pl>
2023-04-25 11:22:49 +0200

    Change temporary StudentsDataDTO class by adding constructor and getRequiredFields method

--- src/types-fe/StudentsDataDTO.ts
+++ src/types-fe/StudentsDataDTO.ts
@@ -1,11 +1,24 @@
 export class StudentsDataDTO {
-    constructor(
-        public email: string,
-        public courseCompletion: boolean,
-        public courseEngagement: number,
-        public projectDegree: number,
-        public teamProjectDegree: number,
-        public bonusProjectUrls: string[],
-        public isActive: boolean
-    ) {}
+    email: string;
+    courseCompletion: number;
+    courseEngagement: number;
+    projectDegree: number;
+    teamProjectDegree: number;
+    bonusProjectUrls: string[] | string;
+    isActive: boolean;
+
+    constructor(newStudentData: StudentsDataDTO) {
+        this.email = newStudentData.email;
+        this.courseCompletion = newStudentData.courseCompletion;
+        this.courseEngagement = newStudentData.courseEngagement;
+        this.projectDegree = newStudentData.projectDegree;
+        this.teamProjectDegree = newStudentData.teamProjectDegree;
+        this.bonusProjectUrls = newStudentData.bonusProjectUrls;
+        this.isActive = newStudentData.isActive;
+    }
+
+    static getRequiredFields() {
+        return ['email', 'courseCompletion', 'courseEngagement',
+            'projectDegree', 'teamProjectDegree', 'bonusProjectUrls'];
+    }
 }
