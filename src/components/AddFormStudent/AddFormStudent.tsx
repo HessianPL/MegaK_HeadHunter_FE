12f0f495e4a60524db6e7919bb44adea6ab1c103
Lena Ka <lena.krol@gmail.com>
2023-04-30 15:05:36 +0200

    Added dir and file for add student form component.

---
+++ src/components/AddFormStudent/AddFormStudent.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AddFormStudent = () => {
+    return <h1 className="theme-text-light">Formularz studenta</h1>
+}
--- src/views/StudentView.tsx
+++ src/views/StudentView.tsx
@@ -1,8 +1,10 @@
 import { LogoutButton } from "../components/common/LogoutButton";
+import {AddFormStudent} from "../components/AddFormStudent/AddFormStudent";
 
 export const StudentView = () => {
 	return <>
 		<h1 className="theme-text-light">Widok kursanta</h1>
 		<LogoutButton/>
+		<AddFormStudent/>
 	</>
 }
