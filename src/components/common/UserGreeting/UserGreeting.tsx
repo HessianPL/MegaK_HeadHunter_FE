96c99608403fb5840ab6f16a6a0697448da95e05
HessianPL <git@lukaszdragon.pl>
2023-05-01 17:23:36 +0200

    Move fetch user logic to the UserGreeting component

--- src/components/common/UserGreeting/UserGreeting.tsx
+++ src/components/common/UserGreeting/UserGreeting.tsx
@@ -1,11 +1,21 @@
-interface Props {
-    userName: string;
-}
+import {useState} from "react";
 
-export const UserGreeting = (props: Props) => {
+export const UserGreeting = () => {
+    const [userName, setUserName] = useState('');
+
+    // useEffect(() => {
+    // 	(async () => {
+    // 		const response = await fetch(`adres API z endpointem, z którego można pobrać user name`, {
+    // 			credentials: 'include',
+    // 		});
+    // 		const user = await response.json;
+    // 		setUserName(user.name);
+    // 	})();
+    // }, []);
+
     return (
             <div className="container-fluid">
-                <h1 className="my-5 ms-5 theme-text-light theme-bg-dark-2">Witaj {props.userName || "na stronie"}!</h1>
+                <h1 className="my-5 ms-5 theme-text-light theme-bg-dark-2">Witaj {userName || "na stronie"}!</h1>
             </div>
     )
 }
