04e7af8ad94a86565c188147c68a194932d9cfd9
HessianPL <git@lukaszdragon.pl>
2023-05-01 20:40:53 +0200

    Extract username from user's e-mail address in UserGreeting component

--- src/components/common/UserGreeting/UserGreeting.tsx
+++ src/components/common/UserGreeting/UserGreeting.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export const UserGreeting = () => {
     const [userName, setUserName] = useState('');
@@ -8,14 +8,15 @@ export const UserGreeting = () => {
     // 		const response = await fetch(`adres API z endpointem, z którego można pobrać user name`, {
     // 			credentials: 'include',
     // 		});
-    // 		const user = await response.json;
-    // 		setUserName(user.name);
+    // 		const user = await response.json();
+    //         const email = user.email;
+    // 		setUserName(email.substring(0, email.indexOf('@')));
     // 	})();
     // }, []);
 
     return (
             <div className="container-fluid">
-                <h1 className="my-5 ms-5 theme-text-light theme-bg-dark-2">Witaj {userName || "na stronie"}!</h1>
+                <h1 className="my-5 ms-5 theme-text-light theme-bg-dark-2">Witaj, {userName || "użytkowniku"}!</h1>
             </div>
     )
 }
