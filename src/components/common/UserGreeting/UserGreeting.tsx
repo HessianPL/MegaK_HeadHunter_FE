8768ac49a70422689775c0e9819583766a1f7189
HessianPL <git@lukaszdragon.pl>
2023-05-01 17:01:35 +0200

    Add UserGreeting component and add it in AdminView

---
+++ src/components/common/UserGreeting/UserGreeting.tsx
@@ -0,0 +1,7 @@
+export const UserGreeting = () => {
+    return (
+            <div className="container-fluid">
+                <h1 className="my-5 ms-5 theme-text-light theme-bg-dark-2">Witaj, Admin!</h1>
+            </div>
+    )
+}
