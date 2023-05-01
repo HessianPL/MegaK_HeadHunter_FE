a1ee133262f966e6465584810b3d888fc2e93236
HessianPL <git@lukaszdragon.pl>
2023-05-01 17:11:39 +0200

    Add userName prop to UserGreeting component and use it in greeting

--- src/components/common/UserGreeting/UserGreeting.tsx
+++ src/components/common/UserGreeting/UserGreeting.tsx
@@ -1,7 +1,11 @@
-export const UserGreeting = () => {
+interface Props {
+    userName: string;
+}
+
+export const UserGreeting = (props: Props) => {
     return (
             <div className="container-fluid">
-                <h1 className="my-5 ms-5 theme-text-light theme-bg-dark-2">Witaj, Admin!</h1>
+                <h1 className="my-5 ms-5 theme-text-light theme-bg-dark-2">Witaj {props.userName || "na stronie"}!</h1>
             </div>
     )
 }
