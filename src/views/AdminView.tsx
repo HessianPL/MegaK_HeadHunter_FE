1bfe288d5a9a62144bcf37b340fbde856ce312da
HessianPL <git@lukaszdragon.pl>
2023-05-01 17:18:23 +0200

    Get user name from API and pass it to UserGreeting component

--- src/views/AdminView.tsx
+++ src/views/AdminView.tsx
@@ -2,11 +2,24 @@ import { AdminMenu } from "../components/admin/AdminMenu";
 import ImportCSV from "../components/admin/ImportCSV";
 import { AddFormHR } from "../components/admin/AddFormHR";
 import {UserGreeting} from "../components/common/UserGreeting/UserGreeting";
+import {useEffect, useState} from "react";
 
 export const AdminView = () => {
+	const [userName, setUserName] = useState('');
+
+	// useEffect(() => {
+	// 	(async () => {
+	// 		const response = await fetch(`adres API z endpointem, z którego można pobrać user name`, {
+	// 			credentials: 'include',
+	// 		});
+	// 		const user = await response.json;
+	// 		setUserName(user.name);
+	// 	})();
+	// }, []);
+
 	return <>
 		<AdminMenu/>
-		<UserGreeting/>
+		<UserGreeting userName={userName}/>
 		<ImportCSV/>
 		<AddFormHR/>
 	</>
