59fbb6af2335c368e046e1dde6fcef5841af50f2
WeronikaSzemi <weronikaszeminska@gmail.com>
2023-05-02 18:43:49 +0200

    created UserContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import { AdminView } from "./views/AdminView";
 import { StudentView } from "./views/StudentView";
 import { RecruiterView } from "./views/RecruiterView";
 import LoginView from "./views/LoginView";
+import { UserContext } from "./contexts/user-context";
 
 function App() {
+    const [id, setId] = useState('');
+    const [role, setRole] = useState(undefined);
+
   return (
-    <Routes>
-      <Route path="/login" element={<LoginView/>}/>
-      <Route path="/admin" element={<AdminView/>}/>
-      <Route path="/student" element={<StudentView/>}/>
-      <Route path="/recruiter" element={<RecruiterView/>}/>
-    </Routes>
+      <UserContext.Provider value={{id, setId, role, setRole}}>
+        <Routes>
+          <Route path="/login" element={<LoginView/>}/>
+          <Route path="/admin" element={<AdminView/>}/>
+          <Route path="/student" element={<StudentView/>}/>
+          <Route path="/recruiter" element={<RecruiterView/>}/>
+        </Routes>
+      </UserContext.Provider>
   );
 }
 
