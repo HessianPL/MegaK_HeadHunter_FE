2546d6bf4cf27191871cb717283fa396b08aa8e9
Lena Ka <lena.krol@gmail.com>
2023-05-05 15:53:50 +0200

    Updated types in StudentCVData.ts

--- src/types-fe/StudentCVData.ts
+++ src/types-fe/StudentCVData.ts
@@ -1,20 +1,34 @@
+export enum ExpectedWorkType {
+    Static = 'Na miejscu',
+    CanMoveOut = 'Gotowość do przeprowadzki',
+    RemoteOnly = 'Wyłącznie zdalnie',
+    Hybrid = 'Hybrydowo',
+    Any = 'Bez znaczenia',
+}
+
+export enum ExpectedContractType {
+    UoPOnly = 'Tylko UoP',
+    B2B = 'Możliwe B2B',
+    UZorUOD = 'Możliwe UZ/UOD',
+    Any = 'Brak preferencji',
+}
 export interface StudentCVData {
     id: string,
     email: string,
-    tel: number,
+    tel: string,
     firstName: string,
     lastName: string,
     githubUsername: string,
     portfolioUrls: string[],
     projectUrls: string[],
-    bio: string,
-    expectedTypeWork: string,
-    targetWorkCity: string,
-    expectedContractTpe: string,
-    expectedSalary: string,
+    bio?: string,
+    expectedTypeWork: ExpectedWorkType,
+    targetWorkCity?: string,
+    expectedContractType: ExpectedContractType,
+    expectedSalary?: number,
     canTakeApprenticeship: boolean,
     monthsOfCommercialExp: number,
-    education: string,
+    education?: string,
     workExperience: string,
-    courses: string,
+    courses?: string,
 }
