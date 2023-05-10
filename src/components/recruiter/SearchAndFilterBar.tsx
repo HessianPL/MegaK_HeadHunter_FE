3b0059273ef089bdb7775dc876b44601c77d0225
Lena Ka <lena.krol@gmail.com>
2023-05-10 14:04:54 +0200

    Improved CSS Style.

--- src/components/recruiter/SearchAndFilterBar.tsx
+++ src/components/recruiter/SearchAndFilterBar.tsx
@@ -12,25 +12,27 @@ export const SearchAndFilterBar = () => {
     console.log(search)
     return (
         <div className="theme-bg-dark-1 p-2 mt-1 ms-2 me-2">
-            <div className="row align-items-center justify-content-between">
-                <form className='col-lg-3 col-md-6 col-sm-12' onChange={setSearchFromLocalState}>
-                    <div className='input-group flex-nowrap'>
+            <div className="row align-items-center">
+                <div className='col-sm-6 d-flex justify-content-start'>
+                    <form onChange={setSearchFromLocalState}>
+                        <div className='input-group flex-nowrap'>
                         <span className='input-group-text'><i
                             className="bi bi-search"/></span>
-                        <input className='form-control-search'
-                               type='text'
-                               name='search'
-                               placeholder='Szukaj...'
-                               value={inputVal}
-                               onChange={e => setInputVal(e.target.value)}
-                        />
-                    </div>
-                </form>
-                <div className='col-md-1 me-2'>
-                    <div className='input-group flex-nowrap'>
-                        <i className="bi bi-funnel-fill theme-bg-dark-4 theme-text-medium-light p-2"/>
-                        <Link to='/recruiter/filter' className="theme-btn-dark-4 p-2">Filtrowanie</Link>
-                    </div>
+                            <input className='form-control-search'
+                                   type='text'
+                                   name='search'
+                                   placeholder='Szukaj...'
+                                   value={inputVal}
+                                   onChange={e => setInputVal(e.target.value)}
+                            />
+                        </div>
+                    </form>
+                </div>
+                <div className='col-sm-6 d-flex justify-content-sm-end justify-content-start my-2'>
+                    <Link to='/recruiter/filter' className="theme-btn-dark-4 p-2">
+                        <i className="bi bi-funnel-fill theme-text-medium-light p-2"/>
+                        Filtrowanie
+                    </Link>
                 </div>
             </div>
         </div>
